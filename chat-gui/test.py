afcf87cca2f6a0174e90d6ee7f1fd484bba450d7
Jesper Christensen <Jesper300595@gmail.com>
2019-11-18 20:53:12 +0100

    Send on Enter, Rating box added, styling done

--- chat-gui/test.py
+++ chat-gui/test.py
@@ -1,48 +1,63 @@
+# Purpose of this file is to get all functions to work. Will be moved to demo.py once its done. There the proper architecture will be done.
 from tkinter import *
+from tkinter import messagebox
 
 string_to_display = []
 
-def show_message():
+def show_message(self):
     # Handling user input
     user_message = input_field.get()
-    show_user_message = "You: {}".format(user_message)
+    show_user_message = 'You: {}'.format(user_message)
     string_to_display.append(show_user_message)
 
     # Handling bot message
     bot_message = input_field.get()
-    show_bot_message = "CVBot: {}".format(bot_message)
+    show_bot_message = 'CVBot: {}'.format(bot_message)
     string_to_display.append(show_bot_message)
 
     # Showing messages
-    text_area = Text(root, height=20, width=40)
+    text_area = Text(root, height=33, width=41)
     text_area.tag_config('bot', foreground='red')
     text_area.tag_config('user', foreground='blue')
 
     tag = ''
     for msg in string_to_display:
-        if msg.startswith('CVBot', 0, 5):
+        if msg.startswith('CVBot', 0, 4):
             tag = 'bot'
         else:
             tag = 'user'
 
+    text_area.insert(END, '\n'.join(string_to_display), tag)
 
-    text_area.insert(END, "\n".join(string_to_display), tag)
+    # Resets the entry widget to be ready for a new message.
+    input_field.delete(0, END)
 
     text_area.grid(row=3, column=3)
-    # label = Label(root, fg='green')
-    # label["text"] = string_to_display
-    # label.grid(row=3, column=3)
+    label = Label(root, fg='green')
+    label.grid(row=0, column=0)
+
+
+def on_close():
+    result = messagebox.askyesno('Rating', 'Were you happy with the conversation?')
+    print(result)
+    # Uncomment for no bad reviews :D
+    # if result == True:
+    #     root.destroy()
+    root.destroy()
 
 root = Tk()
 
-input_field = Entry(root, height=20, width=40)
-button = Button(root, text="Send", command=show_message)
+input_field = Entry(root, width='45')
+button = Button(root, text='Send', command=show_message, width='5')
 
-#input_field.grid(row=1, column=2)
-#button.grid(row=2, column=2)
+# Binds the enter key to the button.
+root.bind('<Return>', show_message)
 
-input_field.place(x=2, y=20)
-button.place(x=30, y=30)
+# input_field.grid(row=1, column=2)
+# button.grid(row=2, column=2)
+button.place(x=292, y=570)
+input_field.place(x=5, y=575)
 
-root.geometry("1000x600")
+root.geometry('345x600')
+root.protocol('WM_DELETE_WINDOW', on_close)
 root.mainloop()
