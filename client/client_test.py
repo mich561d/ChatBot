f23a1c89f8bf304f95cceec489e0f0753403822f
Jesper Christensen <jc@Danskbilferie.local>
2019-12-05 11:41:48 +0100

    integration test of client server

--- client/client_test.py
+++ client/client_test.py
@@ -2,14 +2,24 @@ import unittest
 import socket
 import threading
 
+from client_chatBot import ServerThreadRead, ServerThreadWrite
+import time
+import clientSettings as settings
+
 def run_fake_server(self):
     # Run a server to listen for a connection and then close it
-    server_sock = socket.socket()
-    server_sock.bind(('127.0.0.1', 7777))
-    server_sock.listen(0)
-    server_sock.accept()
-    server_sock.close()
+    readSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    readSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    readSocket.bind(('', settings.TCP_PORT_READ))
+    readSocket.listen(6)
+    readSocket.accept()
+
 
 def test_client_with_fake_server(self):
-    server_thread = threading.Thread(target=self.run_fake_server)
-    cli
+    self.run_fake_server()
+    readSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    readSocket.connect((settings.TCP_IP, settings.TCP_PORT_WRITE))
+
+    threadRead = ServerThreadWrite(readSocket)
+    threadRead.daemon = True
+    threadRead.start()
