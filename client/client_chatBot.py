7d1c84625807bc0fa964f18f39361877837ed2ec
Michael Due Pedersen <mdp@Danskbilferie.local>
2019-12-08 16:08:04 +0100

    Updated README, Updated Client, Refactored minor things

--- client/client_chatBot.py
+++ client/client_chatBot.py
@@ -19,60 +19,86 @@ class ServerThreadWrite(Thread):
     def run(self):
         while True:
             while settings.accept_input:
-                clientInput = input('{}: '.format(settings.USER_NAME))
-                self.socket.send(str.encode(clientInput))
-                if clientInput == 'exit':
-                    print(settings.QUIT)
-                    status = 0
-                    sys.exit()
+                current_question = get_question()
+                if current_question != None:
+                    clientInput = '{}: {}'.format(settings.USER_NAME, current_question))
+                    self.socket.send(str.encode(clientInput))
+                    if clientInput == 'exit':
+                        print(settings.QUIT)
+                        status = 0
+                        sys.exit()
+                    if status == 0:
+                        print(settings.QUIT)
+                        sys.exit()
+                else:
+                    time.sleep(1)
 
 
 class ServerThreadRead(Thread):
 
-    def __init__(self, socket):
+    def __init__(self, socket, readSocket):
         Thread.__init__(self)
         self.socket = socket
+        self.readSocket = readSocket
 
     def run(self):
-        WELCOME_MESSAGE = readSocket.recv(settings.BUFFER_SIZE)
+        WELCOME_MESSAGE = self.readSocket.recv(settings.BUFFER_SIZE)
         print('{}: {}'.format(settings.BOT_NAME, WELCOME_MESSAGE.decode('utf-8')))
         settings.accept_input = True
         while True:
-            chat = readSocket.recv(settings.BUFFER_SIZE)
+            chat = self.readSocket.recv(settings.BUFFER_SIZE)
             print('{}: {}'.format(settings.BOT_NAME, chat.decode('utf-8')))
             if status == 0:
                 sys.exit()
-            time.sleep(5)
+            time.sleep(1)
 
-
-writeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-writeSocket.connect((settings.TCP_IP, settings.TCP_PORT_WRITE))
-readSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-readSocket.connect((settings.TCP_IP, settings.TCP_PORT_READ))
 
 global status
 status = 1
 
-threads = []
-try:
-    writeThread = ServerThreadWrite(writeSocket)
-    readThread = ServerThreadRead(writeSocket)
-    writeThread.daemon = True
-    readThread.daemon = True
-    writeThread.start()
-    readThread.start()
+lock = threading.Lock()
 
-    threads.append(writeThread)
-    threads.append(readThread)
+questions = []
+answers = []
 
-    while True:
-        for t in threads:
-            t.join(600)
-            if not t.isAlive():
-                break
-        break
-except KeyboardInterrupt:
-    clientInput = 'exit'
-    print(settings.QUIT)
-    writeSocket.send(clientInput)
-    sys.exit()
+def add_question(self, question):
+    lock.acquire()
+    questions.append(question)
+    lock.release()
+
+def get_question(self):
+    lock.acquire()
+    current_question = questions.pop(0)
+    lock.release()
+    return current_question
+
+
+def setup(self):
+    writeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    writeSocket.connect((settings.TCP_IP, settings.TCP_PORT_WRITE))
+    readSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    readSocket.connect((settings.TCP_IP, settings.TCP_PORT_READ))
+
+    threads = []
+    try:
+        writeThread = ServerThreadWrite(writeSocket)
+        readThread = ServerThreadRead(writeSocket, readSocket)
+        writeThread.daemon = True
+        readThread.daemon = True
+        writeThread.start()
+        readThread.start()
+
+        threads.append(writeThread)
+        threads.append(readThread)
+
+        while True:
+            for t in threads:
+                t.join(600)
+                if not t.isAlive():
+                    break
+            break
+    except KeyboardInterrupt:
+        clientInput = 'exit'
+        print(settings.QUIT)
+        writeSocket.send(clientInput)
+        sys.exit()
--- client/client_gui.py
+++ client/client_gui.py
@@ -1,15 +1,16 @@
-# Purpose of this file is to get all functions to work. Will be moved to demo.py once its done. There the proper architecture will be done.
 from tkinter import *
-from tkinter import messagebox
-from random import randint as r
+from tkinter import simpledialog
+import client_chatBot as client
 
 string_to_display = []
+
 
 def show_message(self):
     # Handling user input
     user_message = input_field.get()
     show_user_message = 'You: {}'.format(user_message)
     string_to_display.append(show_user_message)
+    client.add_question(user_message)
 
     # Handling bot message
     bot_message = input_field.get()
@@ -18,17 +19,7 @@ def show_message(self):
 
     # Showing messages
     text_area = Text(root, height=33, width=41)
-    text_area.tag_config('bot', foreground='red')
-    text_area.tag_config('user', foreground='blue')
-
-    tag = ''
-    for msg in string_to_display:
-        if msg.startswith('CVBot', 0, 4):
-            tag = 'bot'
-        else:
-            tag = 'user'
-
-    text_area.insert(END, '\n'.join(string_to_display), tag)
+    text_area.insert(END, '\n\n'.join(string_to_display))
 
     # Resets the entry widget to be ready for a new message.
     input_field.delete(0, END)
@@ -38,18 +29,20 @@ def show_message(self):
     label.grid(row=0, column=0)
 
 
-def on_close():
-    result = messagebox.askyesno('Rating', 'Were you happy with the conversation?')
-    rating = 0
-    if result == True:
-        rating = r(3,5)
-    else:
-        rating = r(1,3)
-    # Uncomment for no bad reviews :D
-    # if result == True:
-    #     root.destroy()
-    print(rating)
-    root.destroy()
+def on_close(self):
+    result = simpledialog.askinteger(
+        "Rating",
+        "How happy were you with the conversation? (Scale: Bad 1-5 Great)",
+        minvalue=1,
+        maxvalue=5
+    )
+    if result != None:
+        client.add_question('Rating: {}'.format(result))
+        client.status = 0
+        root.destroy()
+
+
+client.setup()
 
 root = Tk()
 
@@ -59,8 +52,6 @@ button = Button(root, text='Send', command=show_message, width='5')
 # Binds the enter key to the button.
 root.bind('<Return>', show_message)
 
-# input_field.grid(row=1, column=2)
-# button.grid(row=2, column=2)
 button.place(x=292, y=570)
 input_field.place(x=5, y=575)
 
