8742d325dddff0e2cf5c1034a140bf67f848cd12
Jesper Christensen <Jesper300595@gmail.com>
2020-01-08 22:48:23 +0100

    added comments to brain

--- server/brain.py
+++ server/brain.py
@@ -17,6 +17,7 @@ def downloadNLTK():
 stemmer = LancasterStemmer()
 date_format = '%Y/%m/%d-%H:%M:%S'
 
+# Opens data file intents.json
 with open('intents.json') as file:
     data = json.load(file)
 
@@ -29,23 +30,32 @@ except:
     docs_x = []
     docs_y = []
 
+    # Preparing data for our model.
     for intent in data['intents']:
         for pattern in intent['patterns']:
+            # Gets every word, word by word. Instead of an entire sentence
             wrds = nltk.word_tokenize(pattern)
+            # Extend list since we already know it a list, instead of looping though and appending.
             words.extend(wrds)
             docs_x.append(wrds)
             docs_y.append(intent['tag'])
 
+        # Get every tag
         if intent['tag'] not in labels:
             labels.append((intent['tag']))
 
+    # Stems the words.        
     words = [stemmer.stem(w.lower()) for w in words if w != '?']
+    # Sorts them and removes duplicates.
     words = sorted(list(set(words)))
     labels = sorted(labels)
 
     training = []
     output = []
 
+    # Bag of words - one hot incoded.
+
+    # How often a tag is represented -> one hot incoded.
     out_empty = [0 for _ in range(len(labels))]
 
     for x, doc in enumerate(docs_x):
@@ -70,17 +80,22 @@ except:
     with open('data.pickle', 'wb') as f:
         pickle.dump((words, labels, training, output), f)
 
+# Model
 
+# Resetting underlying data graph
 tf.reset_default_graph()
 
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
+net = tflearn.input_data(shape=[None, len(training[0])]) # Input data, som er længden af vores data (patterns)
+net = tflearn.fully_connected(net, 8) # Hidden layer 1
+net = tflearn.fully_connected(net, 8) # Hidden layer 2
+net = tflearn.fully_connected(net, len(output[0]), activation='softmax') # Output layer (tags)
 net = tflearn.regression(net)
 
-model = tflearn.DNN(net)
+model = tflearn.DNN(net) # Model wrapper that can perform classifier tasks.
 
+# Model done
+
+# Training the model
 current_date = dt.datetime.now()
 if current_date.day == 1 or 1 == 1:
     start_time = dt.datetime.now().strftime(date_format)
@@ -118,7 +133,7 @@ else:
                   batch_size=8, show_metric=True)
         model.save('model.tflearn')
 
-
+# Bag of words for the input from user
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
     s_words = nltk.word_tokenize(s)
